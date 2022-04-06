9ede0ab1bb14772ca1ef535793df6d7617a431c0
jodiiiiii <103085992+jodiiiiii@users.noreply.github.com>
2022-04-06 11:08:47 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream> 
+using namespace std;
+int main() {//begin of function
+    float angka;
+    string nama; //deklarasi variabel
+    nama = "Jodi Aditya Bawimbang"; //masukan value kedalam variabel
+    angka = 10.21+3.14;
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<angka;
+    
+}//end of function
